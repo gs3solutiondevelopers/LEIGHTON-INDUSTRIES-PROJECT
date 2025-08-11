a19d2896bdf665638f5f2b92b299db7543201ef9
MdAfnanNaushad <mdafnannaushad@gmail.com>
2025-08-11 16:30:53 +0530

    Added draft Frontend

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/layout/Navbar'
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import HomePage from './pages/HomePage'
+import ContactPage from './pages/ContactPage'
+import ProductsPage from './pages/ProductsPage'
+const App = () => {
+  return (
+     <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+   
+            <Route path="/batteries" element={<ProductsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
