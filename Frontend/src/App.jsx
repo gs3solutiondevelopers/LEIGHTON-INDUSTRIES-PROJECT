8c1fbb3dbdae9519375daa9ba609061ec17afb55
MdAfnanNaushad <mdafnannaushad@gmail.com>
2025-08-14 19:30:53 +0530

    Added changes in the images importing method Added deaoler Pages supporty pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,38 +1,40 @@
-import React from 'react'
-import Navbar from './components/layout/Navbar'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-import Footer from './components/layout/Footer'
-import HomePage from './pages/HomePage'
-import ContactPage from './pages/ContactPage'
-import ProductsPage from './pages/ProductsPage'
-import WhatsApp from './components/common/WhatsApp'
-import CookieBanner from './components/common/CookieBanner'
-import WarrantyPage from './pages/WarrantyPage'
-import SupportPage from './pages/Support'
-import Complaint from './pages/Complain'
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/layout/Footer";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import ProductsPage from "./pages/ProductsPage";
+import WhatsApp from "./components/common/WhatsApp";
+import CookieBanner from "./components/common/CookieBanner";
+import WarrantyPage from "./pages/WarrantyPage";
+import SupportPage from "./pages/Support";
+import Complaint from "./pages/Complain";
+import Dealer from "./pages/Dealer";
 const App = () => {
   return (
-     <Router>
+    <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
-   
+
             <Route path="/batteries" element={<ProductsPage />} />
             <Route path="/contact" element={<ContactPage />} />
-          <Route path="/support" element={<SupportPage />} />
-          {/* supprt routes */}
-            <Route path='support/warranty' element={<WarrantyPage/>}/>
-            <Route path='support/complaint' element={<Complaint/>}/>
+            <Route path="/support" element={<SupportPage />} />
+            {/* supprt routes */}
+            <Route path="support/warranty" element={<WarrantyPage />} />
+            <Route path="support/complaint" element={<Complaint />} />
+            <Route path="support/dealer" element={<Dealer />} />
           </Routes>
         </main>
         <Footer />
         <WhatsApp />
-        <CookieBanner/>
+        <CookieBanner />
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
