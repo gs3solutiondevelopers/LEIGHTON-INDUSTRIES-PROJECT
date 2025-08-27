522111718d62c05510d8b561abbe5fef7551bd02
MdAfnanNaushad <mdafnannaushad@gmail.com>
2025-08-27 19:43:16 +0530

    Added backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,9 @@ import cookieParser from "cookie-parser"
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+  origin: ['https://leighton-industries-project.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
